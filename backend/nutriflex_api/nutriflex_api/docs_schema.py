d8def9e5e1ab73501955d3c3f06ace59766a0952
JamiCode <jamikeohia@gmail.com>
2024-01-14 19:36:27 +0400

    api description edited
    edited api description

--- backend/nutriflex_api/nutriflex_api/docs_schema.py
+++ backend/nutriflex_api/nutriflex_api/docs_schema.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Nutriflex  API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
