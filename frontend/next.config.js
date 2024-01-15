2a63611c0bee8d85488ebcadc7eeb0c3e200ab32
JamiCode <jamikeohia@gmail.com>
2024-01-15 21:02:06 +0400

    Init
    Init

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    API_URL: process.env.API_URL,
-  },
+const nextConfig = {}
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.API_URL}/api/:path*`,
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
--- frontend/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-"use client";
-import AuthProvider from "@/components/auth/AuthProvider";
-// import MainLayout from "@/app/layout";
-
-const home = () => {
-  return <AuthProvider>{/* <MainLayout /> */}</AuthProvider>;
-};
-
-export default home;
