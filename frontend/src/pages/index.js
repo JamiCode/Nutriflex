14a97e18e2b6f36809f56bec6f918f057b2d3c68
JamiCode <jamikeohia@gmail.com>
2024-01-16 12:40:23 +0400

    edited gitingore and fixed minor changes

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is the homepage of my Next.js app.</p>
+    </div>
+  );
+};
+
+export default HomePage;
