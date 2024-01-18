3c9addf36667ddffc65759e542621ec8b0f1d00c
cmastrangelo <carlos.mastrangelo@gmail.com>
2024-01-17 17:58:08 -0800

    chore: improvement in clarifai_debug.py for testing purposes

--- backend/clarifai_utils/clarifai_debug.py
+++ backend/clarifai_utils/clarifai_debug.py
@@ -2,16 +2,18 @@ from clarifai_client import create_new_tasks, update_tasks, build_instruction_se
 import json
 
 if __name__ == '__main__':
-    user_data = {'age': 25, 'weight': 40, 'height': 190, "goals": ["become a professional bodybuilder", "gain muscle"]}
+    user_data = {'age': 25, 'weight': 120, 'height': 190, "goals": ["become a better sumo wrestler", "gain mass"]}
     #print(build_instruction_set('create', user_data))
-    previous_tasks = create_new_tasks(user_data)['tasks']
+    tasks = create_new_tasks(user_data)['tasks']
     #print(json.dumps(tasks, indent=4))
 
     week_report = {
-        'completed_tasks': '3/7',
-        'comment': "It was too hard and I'm too sore."
+        'completed_tasks': '7/7',
+        'week_weight_change': '+1kg',
+        'month_weight_change': '+3kg',
+        'comment': "I had trouble keeping up with tasks last week because I was really stressed out from work"
     }
 
     # print(build_instruction_set('update', user_data, previous_tasks, week_report))
-    updated_tasks = update_tasks(user_data, previous_tasks, week_report)
+    updated_tasks = update_tasks(user_data, tasks, week_report)
     print(json.dumps(updated_tasks, indent=4))
