fcd4f5d4719fb66dac951ec521c926b714157411
JamiCode <jamikeohia@gmail.com>
2024-01-22 00:42:00 +0400

    fixed authenticated navbar

--- frontend/src/components/AuthenticatedNavBar.js
+++ frontend/src/components/AuthenticatedNavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { pm } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../app/globals.css";
