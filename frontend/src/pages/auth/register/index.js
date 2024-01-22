7e90388bad06f35c39ef7b73c53adadab0d50178
JamiCode <jamikeohia@gmail.com>
2024-01-22 11:40:30 +0400

    changed url post params

--- frontend/src/pages/auth/register/index.js
+++ frontend/src/pages/auth/register/index.js
@@ -60,7 +60,7 @@ const SignUp = () => {
     };
     try {
       const response = await fetch(
-        "https://nutriflex-ai.up.railway.app/users/create",
+        "https://nutriflex-ai.up.railway.app/api/users/create",
         fetchObject
       );
       if (!response.ok) {
